3e179d92114ef302d93dd8887ad2ffd3dd19382b
Moahmmed Thaabit <mohammedthaabitjacobs@gmail.com>
2020-07-05 14:19:44 +0200

    asknotes method and tested

--- src/main/java/net/school/persons/learners/Learner.java
+++ src/main/java/net/school/persons/learners/Learner.java
@@ -1,10 +1,19 @@
 package net.school.persons.learners;
 
+import java.time.LocalTime;
 import java.util.ArrayList;
+import java.util.EnumMap;
+import java.util.Map;
+import java.util.Set;
 
+import net.school.curriculum.lessons.AfrikaansLesson;
+import net.school.curriculum.lessons.EnglishLesson;
 import net.school.curriculum.lessons.Lesson;
+import net.school.curriculum.lessons.MathLesson;
+import net.school.curriculum.notes.AquiredType;
 import net.school.curriculum.subjects.Subject;
 import net.school.persons.Person;
+import net.school.persons.teachers.Teacher;
 
 public class Learner extends Person {
 	
@@ -14,11 +23,15 @@ public class Learner extends Person {
 	
 	private boolean attendingLesson;
 	
+	private EnumMap<Subject, AquiredType> notes;
+	
 	public Learner(String firstName, String lastName, String email, ArrayList<Subject> registeredSubjects) {
 		
 		super(firstName, lastName, email);
 		
 		this.registeredSubjects = registeredSubjects;
+		
+		notes = new EnumMap<Subject, AquiredType>(Subject.class);
 	}
 	
 	public boolean getAttendingLesson() {
@@ -26,7 +39,7 @@ public class Learner extends Person {
 	}
 	
 	public void setAttendingLesson(boolean attending) {
-		attendingLesson = true;
+		attendingLesson = attending;
 	}
 	
 	public int getTokens() {
@@ -44,10 +57,57 @@ public class Learner extends Person {
 			if(lesson.getSubject() == sub) {
 				setAttendingLesson(true);
 				lesson.addLearnerLesson(this);
+				
+				notes.put(sub, AquiredType.ATTENDED_LESSON);
+				
 				tokens += 3; 
 				return this.getFullName() + " attending lesson";
 			}
 		
 		return this.getFullName() + " not registered for lesson";
+	}
+	
+	public EnumMap<Subject, AquiredType> getNotes(){
+		return notes;
+	}
+	
+	public String askNotes(Learner learner, Subject subject) {
+		Set<Map.Entry<Subject, AquiredType>> set = learner.getNotes().entrySet();
+		
+		for(Map.Entry<Subject, AquiredType> mapEntry: set) 
+			if(mapEntry.getKey() == subject) 
+				for(Subject registeredsub: registeredSubjects)
+					if(registeredsub == subject) {
+						if(tokens < 2)
+							return "Not Enough tokens";
+						else {
+							tokens -= 2;
+							notes.put(subject, AquiredType.BOUGHT);
+							return "Bought notes for " + subject.toString() + " cost 2 tokens";
+						}
+						
+					} else {
+						if(tokens < 5)
+							return "Not Enough tokens";
+						else {
+							tokens -= 5;
+							notes.put(subject, AquiredType.BOUGHT);
+							return "Bought notes for " + subject.toString() + " cost 5 tokens";
+					}
+				}
+		
+		return learner.getFullName() + " has no notes on " + subject.toString();
+	}
+	
+	public String status() {
+		Set<Map.Entry<Subject, AquiredType>> set = notes.entrySet();
+		String status = "";
+		
+		for(Map.Entry<Subject, AquiredType> me: set)
+			status += me.getKey() + " : " + me.getValue() + "\n"; 
+		
+		status += "Tokens : " + tokens;
+		
+		return status;
 	}
 }
--- src/test/java/net/school/persons/learners/LearnerTest.java
+++ src/test/java/net/school/persons/learners/LearnerTest.java
@@ -7,7 +7,9 @@ import java.util.ArrayList;
 
 import org.junit.jupiter.api.Test;
 
+import net.school.curriculum.lessons.EnglishLesson;
 import net.school.curriculum.lessons.Lesson;
+import net.school.curriculum.notes.AquiredType;
 import net.school.curriculum.subjects.Subject;
 import net.school.persons.teachers.Teacher;
 
@@ -101,5 +103,155 @@ class LearnerTest {
 		
 		assertEquals(3, learn.getTokens());
 	}
-
+	
+	@Test//
+	void shouldReturnEnglishNotesForAttendingEnglishLesson() {
+		ArrayList<Subject> subjects = new ArrayList<>();
+		subjects.add(Subject.ENGLISH);
+		subjects.add(Subject.AFRIKAANS);
+		subjects.add(Subject.LIFE_SCIENCES);
+		
+		Learner learn = new Learner("thaabit", "jacobs", " ", subjects);
+		
+		Teacher teach = new Teacher("Thaabit", "Jacobs", " ", subjects);
+		Lesson less = new Lesson(teach, LocalTime.now(), Subject.ENGLISH);
+		
+		learn.attendLesson(less);
+		
+		assertEquals(3, learn.getTokens());
+	}
+	
+	@Test
+	void shouldReturnLearnerNotRegisesteredForSubject() {
+		ArrayList<Subject> subjectsLearner1 = new ArrayList<>();
+		subjectsLearner1.add(Subject.ENGLISH);
+		Learner Learner1 = new Learner("thaabit", "jacobs", " ", subjectsLearner1);
+		
+		ArrayList<Subject> subjectsLearner2 = new ArrayList<>();
+		subjectsLearner2.add(Subject.MATH);
+		Learner Learner2 = new Learner("Jasn", "Jame", " ", subjectsLearner2);
+		
+		
+		assertEquals("Thaabit Jacobs has no notes on MATH", Learner2.askNotes(Learner1, Subject.MATH));
+	}
+	
+	@Test
+	void shouldReturnNotEnpughTokensEvenThoughLeanerIsRgisteredForSubject() {
+		ArrayList<Subject> subjectsLearner1 = new ArrayList<>();
+		subjectsLearner1.add(Subject.ENGLISH);
+		Learner Learner1 = new Learner("thaabit", "jacobs", " ", subjectsLearner1);
+		Learner1.getNotes().put(Subject.ENGLISH, AquiredType.BOUGHT);
+		
+		ArrayList<Subject> subjectsLearner2 = new ArrayList<>();
+		subjectsLearner2.add(Subject.ENGLISH);
+		Learner Learner2 = new Learner("Jasn", "Jame", " ", subjectsLearner2);
+		
+		
+		assertEquals("Not Enough tokens", Learner2.askNotes(Learner1, Subject.ENGLISH));
+	}
+	
+	@Test
+	void shouldReturnNotEnpughTokensForSubjectLearnerIsNotRegisteredFor() {
+		ArrayList<Subject> subjectsLearner1 = new ArrayList<>();
+		subjectsLearner1.add(Subject.ENGLISH);
+		Learner Learner1 = new Learner("thaabit", "jacobs", " ", subjectsLearner1);
+		Learner1.getNotes().put(Subject.ENGLISH, AquiredType.BOUGHT);
+		
+		ArrayList<Subject> subjectsLearner2 = new ArrayList<>();
+		subjectsLearner2.add(Subject.MATH);
+		Learner Learner2 = new Learner("Jasn", "Jame", " ", subjectsLearner2);
+		
+		
+		assertEquals("Not Enough tokens", Learner2.askNotes(Learner1, Subject.ENGLISH));
+	}
+	
+	@Test
+	void shouldBuyNotesFromLearner() {
+		ArrayList<Subject> subjectsLearner1 = new ArrayList<>();
+		subjectsLearner1.add(Subject.ENGLISH);
+		Learner Learner1 = new Learner("thaabit", "jacobs", " ", subjectsLearner1);
+		Learner1.getNotes().put(Subject.ENGLISH, AquiredType.BOUGHT);
+		
+		ArrayList<Subject> subjectsLearner2 = new ArrayList<>();
+		subjectsLearner2.add(Subject.ENGLISH);
+		subjectsLearner2.add(Subject.AFRIKAANS);
+		subjectsLearner2.add(Subject.BUSSINESS_STUDIES);
+		Learner Learner2 = new Learner("Jasn", "Jame", " ", subjectsLearner2);
+		
+		Teacher teach = new Teacher("Thaabit", "Jacobs", " ", subjectsLearner1);
+		Lesson lesson = new EnglishLesson(teach, LocalTime.now());
+		
+		Learner2.attendLesson(lesson);
+		
+		assertEquals("Bought notes for ENGLISH cost 2 tokens", Learner2.askNotes(Learner1, Subject.ENGLISH));
+	}
+	
+	@Test
+	void shouldDecrementTokensBy2ForBuyingNotesOnRegisteredSubjects() {
+		ArrayList<Subject> subjectsLearner1 = new ArrayList<>();
+		subjectsLearner1.add(Subject.ENGLISH);
+		Learner Learner1 = new Learner("thaabit", "jacobs", " ", subjectsLearner1);
+		Learner1.getNotes().put(Subject.ENGLISH, AquiredType.BOUGHT);
+		
+		ArrayList<Subject> subjectsLearner2 = new ArrayList<>();
+		subjectsLearner2.add(Subject.ENGLISH);
+		subjectsLearner2.add(Subject.AFRIKAANS);
+		subjectsLearner2.add(Subject.BUSSINESS_STUDIES);
+		Learner Learner2 = new Learner("Jasn", "Jame", " ", subjectsLearner2);
+		
+		Teacher teach = new Teacher("Thaabit", "Jacobs", " ", subjectsLearner1);
+		Lesson lesson = new EnglishLesson(teach, LocalTime.now());
+		
+		Learner2.attendLesson(lesson);
+		Learner2.askNotes(Learner1, Subject.ENGLISH);
+		
+		assertEquals(1, Learner2.getTokens());
+	}
+	
+	@Test
+	void shouldBuyNotesFromLearnerUnRegisteredSubjects() {
+		ArrayList<Subject> subjectsLearner1 = new ArrayList<>();
+		subjectsLearner1.add(Subject.ENGLISH);
+		Learner Learner1 = new Learner("thaabit", "jacobs", " ", subjectsLearner1);
+		Learner1.getNotes().put(Subject.LIFE_SCIENCES, AquiredType.BOUGHT);
+		
+		ArrayList<Subject> subjectsLearner2 = new ArrayList<>();
+		subjectsLearner2.add(Subject.ENGLISH);
+		subjectsLearner2.add(Subject.AFRIKAANS);
+		subjectsLearner2.add(Subject.BUSSINESS_STUDIES);
+		Learner Learner2 = new Learner("Jasn", "Jame", " ", subjectsLearner2);
+		
+		Teacher teach = new Teacher("Thaabit", "Jacobs", " ", subjectsLearner1);
+		Lesson lesson = new EnglishLesson(teach, LocalTime.now());
+		
+		Learner2.attendLesson(lesson);
+		Learner2.setAttendingLesson(false);
+		Learner2.attendLesson(lesson);
+		
+		assertEquals("Bought notes for LIFE_SCIENCES cost 5 tokens", Learner2.askNotes(Learner1, Subject.LIFE_SCIENCES));
+	}
+	
+	@Test
+	void shouldDecrementTokenBy5WhenBuyingNotesForUnregisteredSubjects() {
+		ArrayList<Subject> subjectsLearner1 = new ArrayList<>();
+		subjectsLearner1.add(Subject.ENGLISH);
+		Learner Learner1 = new Learner("thaabit", "jacobs", " ", subjectsLearner1);
+		Learner1.getNotes().put(Subject.LIFE_SCIENCES, AquiredType.BOUGHT);
+		
+		ArrayList<Subject> subjectsLearner2 = new ArrayList<>();
+		subjectsLearner2.add(Subject.ENGLISH);
+		subjectsLearner2.add(Subject.AFRIKAANS);
+		subjectsLearner2.add(Subject.BUSSINESS_STUDIES);
+		Learner Learner2 = new Learner("Jasn", "Jame", " ", subjectsLearner2);
+		
+		Teacher teach = new Teacher("Thaabit", "Jacobs", " ", subjectsLearner1);
+		Lesson lesson = new EnglishLesson(teach, LocalTime.now());
+		
+		Learner2.attendLesson(lesson);
+		Learner2.setAttendingLesson(false);
+		Learner2.attendLesson(lesson);
+		Learner2.askNotes(Learner1, Subject.LIFE_SCIENCES);
+		
+		assertEquals(1, Learner2.getTokens());
+	}
 }
