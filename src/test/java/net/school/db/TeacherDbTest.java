1d43a2ac37156ac84096a3dee1434d385a26bfc5
ammaar345 <fakirammaar8@gmail.com>
2020-08-24 13:58:53 +0200

    add teacherdb

---
+++ src/test/java/net/school/db/TeacherDbTest.java
@@ -0,0 +1,14 @@
+package net.school.db;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TeacherDbTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
