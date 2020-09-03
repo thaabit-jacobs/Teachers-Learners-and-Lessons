1de5c12ec6e15cbb6522215a8337a8874e5f4270
ammaar345 <fakirammaar8@gmail.com>
2020-09-03 15:53:11 +0200

    cafemang

---
+++ src/main/java/net/school/db/CafeteriaManagerDb.java
@@ -0,0 +1,5 @@
+package net.school.db;
+
+public class CafeteriaManagerDb {
+
+}
---
+++ src/test/java/net/school/db/CafeteriaManagerDbTest.java
@@ -0,0 +1,14 @@
+package net.school.db;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class CafeteriaManagerDbTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
